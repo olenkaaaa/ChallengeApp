91156d3d854e750e1f87d5469d9d5bf65a09763d
olenkaaaa <lepeckaaleksandra@gmail.com>
2023-06-11 14:43:28 +0200

    Zadanie domowe - dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Napisz w Program.cs kod, w którym:
+// - zadeklarujesz zmienną z imieniem
+// - zadeklarujesz zmienną z płcią (dobierz zmienną)
+// - zadeklarujesz zmienną z wiekiem 
+// - zweryfikujesz dane i wyświetlisz jeden z komunikatów 
+// (1): "Kobieta poniżej 30 lat"
+// (2): "Ewa, lat 33"
+// (3): "Niepełnoletni Mężczyzna"
+// ------------------
+// (możesz dodać swoje)
+
+var name = "Ewa";
+var sex = "Kobieta";
+var age = 33;
+
+if(sex == "Kobieta")
+{
+    if(age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if(age == 30)
+    {
+        Console.WriteLine("Kobieta w wieku 30 lat");
+    }
+    else if(age == 33)
+    {
+        if(name == "Ewa")
+        {
+            Console.WriteLine("Ewa, lat 33");
+        }
+    }
+}
+else if(sex == "Mężczyzna")
+{
+    if(age < 18)
+    {
+        Console.WriteLine("Niepełnoletni Mężczyzna");
+    }
+    else if(name == "Jan" && age == 15)
+    {
+        Console.WriteLine("Jan, lat 15");
+    }
+    else
+    {
+        Console.WriteLine("Pełnoletni Mężczyzna");
+    }
+}
