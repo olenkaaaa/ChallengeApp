a6b28d6c6f7ecb17c3a98bb918af3a7eeb5346bc
olenkaaaa <lepeckaaleksandra@gmail.com>
2023-06-18 11:44:33 +0200

    Klasa Employee

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,30 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+        private List<int> points = new List<int>();
+        public Employee(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+        public string Name { get; private set; }
+
+        public string Surname { get; private set; }
+
+        public int Age { get; private set; }
+
+        public int Points
+        {
+            get
+            {
+                return this.points.Sum();
+            }
+        }
+        public void AddPoints(int point)
+        {
+            this.points.Add(point);
+        }
+    }
+}
