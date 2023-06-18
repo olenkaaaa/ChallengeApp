0ccbe0560350a52b935c3074cca465f04c40edc1
olenkaaaa <lepeckaaleksandra@gmail.com>
2023-06-18 11:45:12 +0200

    Zadanie domowe - dzień 6 (lista, pętla foreach)

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,13 +1,51 @@
-﻿using ChallengeApp;
+﻿// 1. Stwórz klasę Employee, w której przechowasz imię, nazwisko, wiek
+// oraz punkty pracownika w postaci liczb całkowitych.
+//
+// 2. Stwórz 3 pracowników i każdemu przydziel 5 ocen z zakresu
+// od 1 do 10.
+//
+// 3. Napisz program, który wyszuka pracownika z najwyższą liczbą ocen,
+// a następnie wyświetli jego dane oraz wynik.
+using ChallengeApp;
 
-User user1 = new User("Adam", "32423fddf");
-User user2 = new User("Monika", "32423fddf");
-User user3 = new User("Zuzia", "32423fddf");
-User user4 = new User("Damian", "32423fddf");
+Employee employee1 = new Employee("Marta", "Kowalska", 30);
+Employee employee2 = new Employee("Krzysztof", "Nowak", 27);
+Employee employee3 = new Employee("Piotr", "Wiśniewski", 49);
 
-user1.AddScore(5);
-user1.AddScore(2);
-var result = user1.Result;
-Console.WriteLine(result);
-var name = User.GameName; // zmienna statyczna (związana z typem)
-// var pi = Math.PI; - to też jest zmienna statyczna 
+employee1.AddPoints(8);
+employee1.AddPoints(6);
+employee1.AddPoints(4);
+employee1.AddPoints(9);
+employee1.AddPoints(5);
+
+employee2.AddPoints(3);
+employee2.AddPoints(6);
+employee2.AddPoints(5);
+employee2.AddPoints(4);
+employee2.AddPoints(2);
+
+employee3.AddPoints(7);
+employee3.AddPoints(6);
+employee3.AddPoints(8);
+employee3.AddPoints(1);
+employee3.AddPoints(4);
+
+Console.WriteLine(employee1.Points + " " + employee2.Points + " " + employee3.Points);
+
+List<Employee> employees = new List<Employee>()
+{
+    employee1, employee2, employee3
+};
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach (var employee in employees)
+{
+    if (employee.Points > maxResult)
+    {
+        employeeWithMaxResult = employee;
+    }
+}
+
+Console.WriteLine("Najwyższy wynik uzyskał " + employeeWithMaxResult.Name + " " + employeeWithMaxResult.Surname + " " + employeeWithMaxResult.Age + " lat: " + employeeWithMaxResult.Points);
