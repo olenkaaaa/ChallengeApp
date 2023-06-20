0b9039f14fde33a493ae68ca32f3818827707025
olenkaaaa <lepeckaaleksandra@gmail.com>
2023-06-20 17:13:53 +0200

    EmployeeTests

---
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
@@ -0,0 +1,23 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class EmployeeTests
+    {
+        [Test]
+        public void WhenEmployeeCollectsTwoScores_ShouldCorrectResult()
+        {
+            // arrange 
+            var employee = new Employee("Anna", "Nowak", 32);
+            employee.AddPoints(8);
+            employee.AddPoints(-2);
+            employee.AddPoints(6);
+            employee.AddPoints(-7);
+            employee.AddPoints(1);
+
+            // act
+            var result = employee.Points;
+
+            // assert
+            Assert.AreEqual(6, result);
+        }
+    }
+}
