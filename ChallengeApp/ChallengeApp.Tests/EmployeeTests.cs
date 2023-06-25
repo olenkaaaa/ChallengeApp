faf4024fc5c81aa715b0b49c1606f04e649b974e
olenkaaaa <lepeckaaleksandra@gmail.com>
2023-06-25 14:52:31 +0200

    EmployeeTests - drugi sposób

--- ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
@@ -4,7 +4,7 @@
     {
         [Test]
 
-        public void WhenUserGetGrades_CheckIfStatisticsAreCorrect()
+        public void WhenUserGetGrades_CheckIfMaxValueIsCorrect()
         {
             var emp1 = new Employee("Dorota", "Kowalska");
             emp1.AddGrade(3);
@@ -14,8 +14,34 @@
             var statistics = emp1.GetStatistics();
 
             Assert.AreEqual(5, statistics.Max);
-            Assert.AreEqual(3, statistics.Min);
-            Assert.AreEqual(4.00, statistics.Average);
+        }
+
+        [Test]
+
+        public void WhenUserGetGrades_CheckIfMinValueIsCorrect()
+        {
+            var emp2 = new Employee("Jan", "Musiał");
+            emp2.AddGrade(10);
+            emp2.AddGrade(2);
+            emp2.AddGrade(6);
+
+            var statistics = emp2.GetStatistics();
+
+            Assert.AreEqual(2, statistics.Min);
+        }
+
+        [Test]
+        public void WhenUserGetGrades_CheckIfAverageValueIsCorrect()
+        {
+            var emp3 = new Employee("Tomasz", "Zięba");
+            emp3.AddGrade(3);
+            emp3.AddGrade(7);
+            emp3.AddGrade(1);
+            emp3.AddGrade(9);
+
+            var statistics = emp3.GetStatistics();
+
+            Assert.AreEqual(5, statistics.Average);
         }
     }
 }
