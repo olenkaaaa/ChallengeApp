28fa1007da7e75ac011f21202b4765991449f9f0
olenkaaaa <lepeckaaleksandra@gmail.com>
2023-06-26 12:46:29 +0200

    W21 - dzień 10 (Klasa Employee, program)

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -16,7 +16,45 @@
 
         public void AddGrade(float grade)
         {
-            this.grades.Add(grade);
+            // 3.99 
+            // 3
+            int valueInInt = (int)grade; // rzutowanie lub Math.Ceiling(grade); / Math.Floor(grade) ;
+            float f = valueInInt; // z inta na floata
+
+            if (grade >= 0 && grade <= 100)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("Invalid grade value");
+            }
+        }
+        public void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float result))
+            {
+                this.AddGrade(result);
+            }
+            else
+            {
+                Console.WriteLine("String is not float");
+            }
+        }
+        public void AddGrade(double grade)
+        {
+            float doubleToFloat = (float)grade;
+            this.AddGrade(doubleToFloat);
+        }
+        public void AddGrade(long grade)
+        {
+            float longToFloat = (float)grade;
+            this.AddGrade(longToFloat);
+        }
+        public void AddGrade(decimal grade)
+        {
+            float decimalToFloat = (float)grade;
+            this.AddGrade(decimalToFloat);
         }
 
         public Statistics GetStatistics()
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,22 +1,15 @@
 ﻿using ChallengeApp;
 
-var employee1 = new Employee("Marcin", "Twardowski");
-employee1.AddGrade(2);
-employee1.AddGrade(2);
-employee1.AddGrade(6);
-var statistics1 = employee1.GetStatistics();
-Console.WriteLine($"{employee1.Name} {employee1.Surname}");
-Console.WriteLine($"Average: {statistics1.Average:N2}");
-Console.WriteLine($"Min: {statistics1.Min}");
-Console.WriteLine($"Max: {statistics1.Max}");
-Console.WriteLine();
+var employee = new Employee("Marcin", "Twardowski");
+employee.AddGrade("Adam");
+employee.AddGrade("4000");
+employee.AddGrade(2);
+employee.AddGrade(6);
+var statistics = employee.GetStatistics();
 
-var employee2 = new Employee("Anna", "Nowak");
-employee2.AddGrade(7);
-employee2.AddGrade(4);
-employee2.AddGrade(1);
-var statistics2 = employee2.GetStatistics();
-Console.WriteLine($"{employee2.Name} {employee2.Surname}");
-Console.WriteLine($"Average: {statistics2.Average:N2}");
-Console.WriteLine($"Min: {statistics2.Min}");
-Console.WriteLine($"Max: {statistics2.Max}");
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Min: {statistics.Min}");
+Console.WriteLine($"Max: {statistics.Max}");
+
+// ctrl + k + c - dodaje komentarze
+// ctrl + k + u - usuwa komentarze 
